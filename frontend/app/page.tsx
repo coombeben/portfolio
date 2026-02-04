f52dc4ee0d14aa0e11925188224f69ff2842d8ec
coomb <coombeben@gmail.com>
2026-02-04 13:49:10 +0000

    Add default frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { CopilotChat } from "@copilotkit/react-ui";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Your App</h1>
+      <CopilotChat />
+    </main>
+  );
+}
