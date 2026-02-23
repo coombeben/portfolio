0e992b4cad1f9cc48610584f81dd6d6e8b3b1ed6
coomb <coombeben@gmail.com>
2026-02-23 16:25:35 +0000

    Update next config for standalone output

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // CORS issue prevention
-  async rewrites() {
-    return [
-      {
-        source: "/api/quota",
-        destination: "http://localhost:8000/chat/quota",
-      },
-      {
-        source: "/auth/session",
-        destination: "http://localhost:8000/auth/session",
-      },
-      {
-        source: "/auth/login",
-        destination: "http://localhost:8000/auth/login",
-      },
-    ];
-  },
+  output: 'standalone'
 };
 
 export default nextConfig;
